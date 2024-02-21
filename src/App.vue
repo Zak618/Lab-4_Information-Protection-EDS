7ccac1305f2d6a92ed2df4e9354a285f4ab5eca1
Rolan <89405318+Zak618@users.noreply.github.com>
2024-02-21 16:49:21 +0300

    Добавил базовый интерфейс

--- src/App.vue
+++ src/App.vue
@@ -1,53 +1,99 @@
+<template>
+  <div id="app">
+    <input v-model="m" @input="inputMessage" type="text" placeholder="Введите текст">
+    <input v-model="e" @input="eKeys" type="text" placeholder="e">
+    <input v-model="n" @input="nKeys" type="text" placeholder="n">
+    <input v-model="encryptedMessage" type="text" placeholder="текст" readonly>
+
+    <input v-model="m1" @input="destMessage" type="text">
+    <input v-model="d" @input="dKeys" type="text">
+    <input v-model="n" @input="nKeys" type="text">
+    <input :value="decryptedMessage" type="text" readonly>
+  </div>
+</template>
+
 <script>
 import { gcd } from 'mathjs';
 
 let SimpleNumbers = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293];
 
-let p = SimpleNumbers[Math.floor(Math.random() * SimpleNumbers.length)];
-let q = SimpleNumbers[Math.floor(Math.random() * SimpleNumbers.length)];
+export default {
+  data() {
+    return {
+      m: '',
+      e: '',
+      n: '',
+      m1: '',
+      d: '',
+      encryptedMessage: '',
+      decryptedMessage: ''
+    }
+  },
 
-let n = p * q;
-let Fn = (p - 1) * (q - 1);
+  created() {
+    this.initializeKeys();
+  },
 
-let e = eFunc(Fn);
+  methods: {
+    initializeKeys() {
+      let p = SimpleNumbers[Math.floor(Math.random() * SimpleNumbers.length)];
+      let q = SimpleNumbers[Math.floor(Math.random() * SimpleNumbers.length)];
 
-function eFunc(Fn) {
-  while (true) {
-    let y = SimpleNumbers[Math.floor(Math.random() * SimpleNumbers.length)];
-    let x = gcd(y, Fn);
-    if (x == 1) {
-      return y;
+      this.n = p * q;
+      let Fn = (p - 1) * (q - 1);
+
+      this.e = this.eFunc(Fn);
+      this.d = this.modInverse(this.e, Fn);
+    },
+
+    eFunc(Fn) {
+      while (true) {
+        let y = SimpleNumbers[Math.floor(Math.random() * SimpleNumbers.length)];
+        let x = gcd(y, Fn);
+        if (x == 1) {
+          return y;
+        }
+      }
+    },
+
+    modInverse(a, m) {
+      for (let i = 0; i < m; i++) {
+        if ((a * i) % m === 1) {
+          return i;
+        }
+      }
+      return 1;
+    },
+
+    inputMessage() {
+      this.updateEncryptedMessage();
+    },
+
+
+    destMessage() {
+      this.updateDescryptedMessage();
+    },
+
+
+    updateEncryptedMessage() {
+      let result1 = '';
+      for (let i = 0; i < this.m.length; i++) {
+        let x = BigInt(this.m.charCodeAt(i));
+        let result = x ** BigInt(this.e) % BigInt(this.n);
+        result1 += result.toString() + ' ';
+      }
+      this.encryptedMessage = result1;
+    },
+
+    updateDescryptedMessage() {
+      let result2 = '';
+      let res = this.encryptedMessage.split(' ');
+      for(let i = 0; i < res.length; i++) {
+        let result = BigInt(res[i]) ** BigInt(this.d) % BigInt(this.n);
+        result2 += String.fromCharCode(Number(result));
+      }
+      this.decryptedMessage = result2;
     }
   }
 }
-
-let d = modInverse(e, Fn);
-
-function modInverse(a, m) {
-  for (let i = 0; i < m; i++) {
-    if ((a * i) % m === 1) {
-      return i;
-    }
-  }
-  return 1;
-}
-
-let l1 = 'Hello world!';
-let result1 = '';
-for(let i = 0; i < l1.length; i++) {
-  let x = BigInt(l1.charCodeAt(i));
-  let result = x ** BigInt(e) % BigInt(n);
-  result1 += result.toString() + ' ';
-}
-
-console.log(result1);
-
-let result2 = '';
-let res = result1.split(' ');
-for(let i = 0; i < res.length; i++) {
-  let result = BigInt(res[i]) ** BigInt(d) % BigInt(n);
-  result2 += String.fromCharCode(Number(result));
-}
-
-console.log(result2);
 </script>
